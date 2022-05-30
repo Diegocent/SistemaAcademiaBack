aa1dc90b15bb720944b2cd41585f0e42036e4167
DiegovillaxD05 <diegovillalbacent1234@gmail.com>
2022-05-30 16:51:20 -0400

    configuracion del server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ db.sequelize.sync();
 
 var corsOptions = {
   origin: "http://localhost:4200",
+  deploy: "http://pykasujeroky.infographicdic.com",
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
